9ef618a54a383a6607990f3b3c41d9951543bdd5
omardieh <omar.dieh@wildcodeschool.com>
2023-07-21 01:46:29 +0200

    user can verify email

--- backend/routes/auth/verify.route.js
+++ backend/routes/auth/verify.route.js
@@ -1,18 +1,34 @@
 const express = require("express");
 const User = require("../../models/User.model");
+const { isAuthenticated } = require("../../middleware/jwt.middleware");
 
 const verifyRouter = express.Router();
 
-verifyRouter.get("/", (req, res, next) => {
+verifyRouter.get("/token", isAuthenticated, (req, res, next) => {
   res.status(200).json(req.payload);
 });
 
-verifyRouter.get("/email", (req, res, next) => {
+verifyRouter.get("/email", (req, res) => {
   const { userID, code } = req.query;
   User.findById(userID).then((foundUser) => {
     if (!foundUser) return;
-    const codeCorrect = foundUser.compareEmail(code);
-    console.log(codeCorrect);
+    foundUser.compareEmail(code).then((isMatch) => {
+      if (!isMatch) {
+        res.status(400).json("verification code is not valid");
+        return;
+      }
+      User.findByIdAndUpdate(
+        foundUser._id,
+        { isEmailVerified: true },
+        { new: true }
+      ).then((updatedUser) => {
+        if (!updatedUser.isEmailVerified) {
+          res.status(400).json("error while verifying email");
+          return;
+        }
+        res.redirect(process.env.CLIENT_URL + "/login");
+      });
+    });
   });
 });
 
