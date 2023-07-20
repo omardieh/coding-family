2d2e6a55006ae560faf76138f2922775eede5fb3
omardieh <omar.dieh@wildcodeschool.com>
2023-07-21 01:46:29 +0200

    user can verify email

--- backend/routes/auth/index.js
+++ backend/routes/auth/index.js
@@ -1,7 +1,6 @@
 const { signupRouter } = require("./signup.route");
 const { loginRouter } = require("./login.route");
 const { verifyRouter } = require("./verify.route");
-const { isAuthenticated } = require("../../middleware/jwt.middleware");
 
 module.exports = (app) => {
   app.use("/auth/signup", signupRouter);
