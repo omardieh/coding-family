ccbbf2f197b47d010f50df63ecf4a4af39cb55a6
Omar Sh. Dieh <omarsheikhdieh@gmail.com>
2023-07-24 17:24:57 +0200

    add default avatar on user signup

--- backend/models/User.model.js
+++ backend/models/User.model.js
@@ -32,6 +32,7 @@ const userSchema = new Schema(
         "Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.",
       ],
     },
+    avatar: { type: String, default: "" },
     role: {
       type: String,
       enum: ["member", "editor", "admin"],
@@ -39,10 +40,11 @@ const userSchema = new Schema(
     },
     emailVerifyCode: {
       type: String,
-      default: "",
+      required: [true, "Email Verify code is missing."],
     },
     emailVerifyCodeExpiresAt: {
       type: Date,
+      required: [true, "Email Verify code exp date is missing."],
     },
     isEmailVerified: {
       type: Boolean,
