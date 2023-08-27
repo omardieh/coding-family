1f2c29ce2e538f5201143457d30143fce8aef35a
omardieh <omar.dieh@wildcodeschool.com>
2023-08-27 20:29:51 +0200

    integrating chat

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 require("dotenv").config();
 const app = require("./app");
 const { connectDB } = require("./config/database.config");
-
-connectDB().then(connectServer);
+const { connectSocketIO } = require("./config/socket.config");
 
 function connectServer() {
-  app.listen(process.env.SEVER_PORT, (e) => {
+  const server = app.listen(process.env.SEVER_PORT, (e) => {
     if (e) {
       console.error("app.listen:", e);
     }
     console.info(`connected Server  | PORT : ${process.env.SEVER_PORT}`);
   });
+  connectSocketIO(server);
 }
+
+connectDB().then(connectServer);
