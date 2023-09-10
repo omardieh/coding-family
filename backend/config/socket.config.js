ceb5e5c672e6d1aba9c66af363f0c76bc046f723
omardieh <omar.dieh@wildcodeschool.com>
2023-09-10 17:30:24 +0200

    test sending socket to client and to server

--- backend/config/socket.config.js
+++ backend/config/socket.config.js
@@ -1,18 +1,29 @@
 const socketIO = require("socket.io");
 
-let io;
-
-function connectSocketIO(server) {
-  io = socketIO(server, {
+function useSocketIO(server) {
+  const io = socketIO(server, {
     cors: { origin: [process.env.CLIENT_URL] },
+  });
+  io.on("connection", (socket) => {
+    console.log("A user connected");
+    socket.on("disconnect", () => {
+      console.log("User disconnected");
+    });
+    const messages = [
+      {
+        author: "server",
+        message:
+          "welcome to the webSocket chat app! mad with Node, React and socket.io",
+      },
+    ];
+    socket.emit("MessagesFromServer", messages);
+    socket.on("MessageToServer", (message) => {
+      messages.push(message);
+      io.emit("MessageToClient", message);
+    });
   });
 }
 
-function useSocketIO() {
-  return io;
-}
-
 module.exports = {
-  connectSocketIO,
   useSocketIO,
 };
