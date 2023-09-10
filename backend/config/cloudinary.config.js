48f97d47a4b9f35a08d892d910ce883f9c82bfd6
omardieh <omar.dieh@wildcodeschool.com>
2023-09-10 19:18:30 +0200

    upload user image

---
+++ backend/config/cloudinary.config.js
@@ -0,0 +1,19 @@
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const multer = require("multer");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    allowed_formats: ["jpg", "png"],
+    folder: "coding-family/user",
+  },
+});
+
+module.exports = multer({ storage });
