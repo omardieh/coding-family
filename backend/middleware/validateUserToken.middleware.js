b979a6d12a298ca6ecd9ea523ef238acd05403dd
Omar Sh. Dieh <omarsheikhdieh@gmail.com>
2024-02-21 15:32:53 +0100

    update validate token approach

---
+++ backend/middleware/validateUserToken.middleware.js
@@ -0,0 +1,11 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const token = req.headers["authorization"];
+  if (!token) return res.status(403).json({ message: "Token not provided" });
+  jwt.verify(token, process.env.JWT_TOKEN_SECRET, (error, decoded) => {
+    if (error) return res.status(401).json({ message: "Unauthorized" });
+    req.payload = { ...decoded, isAuthenticated: true };
+    next();
+  });
+};
