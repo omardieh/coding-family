ba1d97e69cc038b0d8f662b106492a949d5727cb
Omar Sh. Dieh <omarsheikhdieh@gmail.com>
2024-02-23 16:52:34 +0100

    updaing navbar with material ui nav

---
+++ frontend/src/components/Navbar/navLinks.js
@@ -0,0 +1,17 @@
+import { v4 as uuid } from "uuid";
+
+export const pagesLinks = [
+  { id: uuid(), title: "Welcome", path: "/" },
+  { id: uuid(), title: "Projects", path: "/projects" },
+  { id: uuid(), title: "Chat", path: "/chat" },
+  { id: uuid(), title: "Dashboard", path: "/account/dashboard" },
+  { id: uuid(), title: "Login / Register", path: "/account" },
+];
+
+export const accountLinks = [
+  { id: uuid(), title: "Welcome", path: "/" },
+  { id: uuid(), title: "Projects", path: "/projects" },
+  { id: uuid(), title: "Chat", path: "/chat" },
+  { id: uuid(), title: "Dashboard", path: "/account/dashboard" },
+  { id: uuid(), title: "Login / Register", path: "/account" },
+];
