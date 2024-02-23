77c59c6367c203f260780bb72176ac22f1cbf452
Omar Sh. Dieh <omarsheikhdieh@gmail.com>
2024-02-23 14:34:12 +0100

    update nav link

--- backend/middleware/validateUserToken.middleware.js
+++ backend/middleware/validateUserToken.middleware.js
@@ -1,8 +1,9 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  const token = req.headers["authorization"];
+  let token = req.headers["authorization"];
   if (!token) return res.status(403).json({ message: "Token not provided" });
+  token = token.split(" ")[1];
   jwt.verify(token, process.env.JWT_TOKEN_SECRET, (error, decoded) => {
     if (error) return res.status(401).json({ message: "Unauthorized" });
     req.payload = { ...decoded, isAuthenticated: true };
