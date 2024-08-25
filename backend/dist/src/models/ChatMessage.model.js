6d9d42072ad9a6e98653343e115298b1bf4f7019
omardieh <omar.dieh@wildcodeschool.com>
2024-08-26 01:52:23 +0200

    fix remove dist

--- backend/dist/src/models/ChatMessage.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ChatMessageModel = void 0;
-const mongoose_1 = require("mongoose");
-const chatMessageSchema = new mongoose_1.Schema({
-    user: { type: mongoose_1.Schema.Types.ObjectId, ref: 'User', required: true },
-    message: { type: String, required: true },
-}, { timestamps: true });
-exports.ChatMessageModel = (0, mongoose_1.model)('ChatMessage', chatMessageSchema);
