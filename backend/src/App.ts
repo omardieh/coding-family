c607aeffd5acc56d775b80bc3d8a9048c1f02b6c
omardieh@gmail.com <omardieh@gmail.com>
2025-02-25 17:54:35 +0100

    fix: reorder initialization of SecurityConfig and EnvironmentConfig for consistency

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -13,8 +13,8 @@ class App {
   }
 
   private initializeConfigs = (): void => {
-    new SecurityConfig(this.app);
     new EnvironmentConfig(this.app);
+    new SecurityConfig(this.app);
     new LoggingConfig(this.app);
   };
 
